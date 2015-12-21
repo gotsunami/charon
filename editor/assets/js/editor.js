7b7c044fa365e807723f701f39685d817cccd092
Anthony <ab@outofpluto.com>
2015-12-21 18:13:12 +0100

    Erase button

--- editor/assets/js/editor.js
+++ editor/assets/js/editor.js
@@ -1,7 +1,7 @@
 $(function() {
     // init editor
     var editor = ace.edit("editor");
-    var autosave;
+    var autosave = undefined;
     editor.setTheme("ace/theme/tomorrow");
     editor.getSession().setMode("ace/mode/yaml");
 
@@ -20,26 +20,36 @@ $(function() {
 	  clearTimeout(autosave);
 	}
 	autosave = setTimeout(saveEditor, 5000);
+	return false;
       });
 
     // bind function on save button
     $("#action-save").click(function() {
 	saveEditor();
+	return false;
       });
 
     // bind function on share button
     $("#action-share").click(function() {
 	window.location = "mailto:?subject=J'aimerais partager ce fichier LIA&body=Voici le lien : " + window.location;
+	return false;
       });
 
     // bind function on + and - buttons
     $("#font-plus, #font-minus").click(function() {
 	$("#editor").css("font-size", (parseFloat($("#editor").css("font-size"))+parseFloat($(this).attr("data-value")))+"px");
+	return false;
       });
 
     // bind function for fullscreen mode
     $('#editor-full').click(function() {
 	$('#editor').fullscreen();
+	return false;
+      });
+
+    // bind function for fullscreen mode
+    $('#editor-clear').click(function() {
+	editor.setValue("models:");
 	return false;
       });
 
